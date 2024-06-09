afd2f7c3091be4c8473ca996fba939d08bdbfcf4
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-06-09 12:46:13 -0300

    feat: Change config vite

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,6 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: [
-    '**/*.glb',
-    '**/*.gltf',
-    '**/*.png',
-    '**/*.jpg',
-    '**/*.jpeg',
-    '**/*.svg',
-    '**/*.css',
-    '**/*.js',
-    '**/*.json',
-  ],
+  assetsInclude: ['**/*.glb'],
   base: '/jigonzalez930209.github.io/',
 })
