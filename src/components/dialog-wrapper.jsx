242e4a95056f491c9e16644c677c1662cfdede5d
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-07-17 20:08:18 -0300

    feat: Change assets and add configs

---
+++ src/components/dialog-wrapper.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+
+const DialogCloseButton = ({ ...props }) => {
+  const { title, content, open, onOpenChange } = props
+  return (
+    <Dialog open={open} onOpenChange={(state) => !state && onOpenChange(null)}>
+      <DialogContent aria-describedby={title} className="sm:max-w-md">
+        <DialogDescription>
+          <span className="sr-only">{title}</span>
+        </DialogDescription>
+        <DialogHeader>
+          <DialogTitle className="text-foreground">{title}</DialogTitle>
+        </DialogHeader>
+        <div className="flex items-center space-x-2 text-foreground">
+          {content}
+        </div>
+        <DialogFooter className="justify-end">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+export default DialogCloseButton
