3991b0a787aef3c297c0c597f99550d78bb6a801
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-07-17 20:12:37 -0300

    style: Change loader color

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -4,7 +4,7 @@ const Loader = () => {
   return (
     <Html>
       <div className="flex justify-center ">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500/20 rounded-full animate-spin" />
+        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-100 border-t-blue-300/10 rounded-full animate-spin" />
       </div>
     </Html>
   )
