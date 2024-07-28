45ffb006f6653c392366c6bf334d2b1c58c86038
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-07-27 22:18:53 -0300

    feat: Add support functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -40,3 +40,53 @@ export const generatePointOnSphere = (center, radius) => {
   const z = center.z + radius * Math.cos(phi)
   return new THREE.Vector3(x, y, z)
 }
+
+export const createRepeatingTextTexture = (
+  text,
+  backgroundColor = new THREE.Color(0x000000),
+  width = 512,
+  height = 512,
+  repeatX = 2,
+  repeatY = 2
+) => {
+  const canvas = document.createElement('canvas')
+  canvas.width = width
+  canvas.height = height
+  const context = canvas.getContext('2d')
+
+  backgroundColor.offsetHSL(0, 0, 0.12).clone()
+  const hexColor = `#${backgroundColor.getHexString()}`
+
+  context.fillStyle = hexColor
+  context.fillRect(0, 0, width, height)
+
+  context.font = 'Bold 100px Arial'
+
+  context.fillStyle = `black`
+  context.textAlign = 'center'
+  context.textBaseline = 'middle'
+
+  for (let i = 0; i <= repeatX; i++) {
+    for (let j = 0; j <= repeatY; j++) {
+      context.save()
+
+      const x = (i + 0.5) * (width / repeatX)
+      const y = (j + 0.5) * (height / repeatY)
+
+      context.translate(x, y)
+
+      const angle = Math.random() * 2 * Math.PI
+      context.rotate(angle)
+
+      context.fillText(text, 0, 0)
+
+      context.restore()
+    }
+  }
+
+  const texture = new THREE.CanvasTexture(canvas)
+  texture.wrapS = THREE.MirroredRepeatWrapping
+  texture.wrapT = THREE.MirroredRepeatWrapping
+  texture.repeat.set(repeatX, repeatY)
+  return texture
+}
