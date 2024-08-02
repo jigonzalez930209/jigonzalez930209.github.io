6aabe2fe63c7321e8c599c5ed4c33ef8f1a56564
José Iván González Jorge <jigonzalez930209@gmail.com>
2024-08-02 14:16:07 -0300

    feat: Add new atoms mirrors in Y axis to restrict camera position

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,7 +7,21 @@ export const cn = (...args) => {
 }
 
 export const findOptimalPoint = (target, radius, atoms) => {
-  const pointsOnSphere = 100
+  const pointsOnSphere = 50
+  const newAtoms = atoms.reduce(
+    (acc, atom) => [
+      ...acc,
+      {
+        ...atom,
+        position: new THREE.Vector3(
+          atom.position.x,
+          atom.position.y - 2,
+          atom.position.z
+        ),
+      },
+    ],
+    [...atoms]
+  )
   let maxMinDistance = -Infinity
   let optimalPoint = null
 
@@ -15,12 +29,10 @@ export const findOptimalPoint = (target, radius, atoms) => {
     const point = generatePointOnSphere(target, radius)
 
     let minDistance = Infinity
-    atoms.forEach((atom) => {
-      if (atom.name.includes('Atom') && atom.name !== target.name) {
-        const distance = point.distanceTo(atom.position)
-        if (distance < minDistance) {
-          minDistance = distance
-        }
+    newAtoms.forEach((atom) => {
+      const distance = point.distanceTo(atom.position)
+      if (distance < minDistance) {
+        minDistance = distance
       }
     })
 
@@ -39,54 +51,4 @@ export const generatePointOnSphere = (center, radius) => {
   const y = center.y + radius * Math.sin(phi) * Math.sin(theta)
   const z = center.z + radius * Math.cos(phi)
   return new THREE.Vector3(x, y, z)
-}
-
-export const createRepeatingTextTexture = (
-  text,
-  backgroundColor = new THREE.Color(0x000000),
-  width = 512,
-  height = 512,
-  repeatX = 2,
-  repeatY = 2
-) => {
-  const canvas = document.createElement('canvas')
-  canvas.width = width
-  canvas.height = height
-  const context = canvas.getContext('2d')
-
-  backgroundColor.offsetHSL(0, 0, 0.12).clone()
-  const hexColor = `#${backgroundColor.getHexString()}`
-
-  context.fillStyle = hexColor
-  context.fillRect(0, 0, width, height)
-
-  context.font = 'Bold 100px Arial'
-
-  context.fillStyle = `black`
-  context.textAlign = 'center'
-  context.textBaseline = 'middle'
-
-  for (let i = 0; i <= repeatX; i++) {
-    for (let j = 0; j <= repeatY; j++) {
-      context.save()
-
-      const x = (i + 0.5) * (width / repeatX)
-      const y = (j + 0.5) * (height / repeatY)
-
-      context.translate(x, y)
-
-      const angle = Math.random() * 2 * Math.PI
-      context.rotate(angle)
-
-      context.fillText(text, 0, 0)
-
-      context.restore()
-    }
-  }
-
-  const texture = new THREE.CanvasTexture(canvas)
-  texture.wrapS = THREE.MirroredRepeatWrapping
-  texture.wrapT = THREE.MirroredRepeatWrapping
-  texture.repeat.set(repeatX, repeatY)
-  return texture
 }
