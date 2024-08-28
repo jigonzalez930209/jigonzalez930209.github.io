d7f4f7f2275f32c89cb455860360d1b8afd3b00e
Jose Ivan <jigonzalez930209@gmail.com>
2024-08-28 13:08:05 -0300

    feat: Add btn to handle a tour

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,6 +12,7 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip'
 import { cn } from '@/utils'
+import { useTour } from './tour-provider'
 
 const NavLinkItems = [
   { content: 'Home', to: '/' },
@@ -43,20 +44,40 @@ const NavItem = ({ selected, disabled = false, content, ...props }) =>
 
 const Navbar = () => {
   const { pathname } = useLocation()
+  const tour = useTour()
+
+  console.log(tour)
 
   return (
     <header className="absolute w-full z-50 top-0 flex text-lg font-medium h-16 justify-between bg-secondary items-center gap-4 px-4 md:px-20 dark:text-slate-400 text-slate-600">
       <TooltipProvider delayDuration={10}>
-        <nav className="hidden flex-col select-none sm:w-full gap-6 sm:flex sm:flex-row items-center justify-center sm:text-sm">
-          {NavLinkItems.map((item) => (
-            <NavItem
-              key={item.content}
-              selected={item.to === pathname}
-              content={item.content}
-              disabled={item.disabled}
-              {...item}
-            />
-          ))}
+        <nav className="hidden select-none sm:w-full gap-6 sm:grid sm:grid-cols-2 items-center justify-center sm:text-sm">
+          <div className="flex sm:flex-row items-center justify-center gap-4 ">
+            {NavLinkItems.map((item) => (
+              <NavItem
+                key={item.content}
+                selected={item.to === pathname}
+                content={item.content}
+                disabled={item.disabled}
+                {...item}
+              />
+            ))}
+          </div>
+          <div className="w-full flex items-center justify-end">
+            <Tooltip>
+              <TooltipTrigger>
+                <div
+                  className="hover:cursor-pointer hover:text-slate-300"
+                  onClick={() => tour.setTourActive(true)}
+                >
+                  take a tour
+                </div>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="animate-pulse">Tour throw app</p>
+              </TooltipContent>
+            </Tooltip>
+          </div>
         </nav>
         <Sheet>
           <SheetTrigger asChild>
