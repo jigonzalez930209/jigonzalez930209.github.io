1e41ae92d779b04e2c2806999dc4c30ed7f83ce5
Jose Ivan <jigonzalez930209@gmail.com>
2024-08-31 10:57:05 -0300

    fix: Solve styles and positions

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-vars */
 import { Menu } from 'lucide-react'
 import { NavLink, useLocation } from 'react-router-dom'
+import React from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
@@ -44,13 +45,14 @@ const NavItem = ({ selected, disabled = false, content, ...props }) =>
 
 const Navbar = () => {
   const { pathname } = useLocation()
+  const [open, setOpen] = React.useState(false)
   const tour = useTour()
 
   return (
     <header className="absolute w-full z-50 top-0 flex text-lg font-medium h-16 justify-between bg-secondary items-center gap-4 px-4 md:px-20 dark:text-slate-400 text-slate-600">
       <TooltipProvider delayDuration={10}>
-        <nav className="hidden select-none sm:w-full gap-6 sm:grid sm:grid-cols-2 items-center justify-center sm:text-sm">
-          <div className="flex sm:flex-row items-center justify-center gap-4 ">
+        <nav className="hidden select-none sm:w-full gap-6 sm:grid sm:grid-cols-3 items-center justify-center">
+          <div className="flex col-span-2 sm:flex-row items-center justify-end gap-4 ">
             {NavLinkItems.map((item) => (
               <NavItem
                 key={item.content}
@@ -77,31 +79,49 @@ const Navbar = () => {
             </Tooltip>
           </div>
         </nav>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button
-              variant="outline"
-              size="icon"
-              className="shrink-0 sm:hidden"
-            >
-              <Menu className="h-5 w-5" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <nav className="grid gap-6">
-              {NavLinkItems.map((item) => (
-                <NavItem
-                  key={item.content}
-                  selected={pathname.includes(item.to)}
-                  content={item.content}
-                  disabled={item.disabled}
-                  {...item}
-                />
-              ))}
-            </nav>
-          </SheetContent>
-        </Sheet>
+        <div className="w-full flex justify-between sm:hidden">
+          <Sheet open={open} onOpenChange={() => setOpen((p) => !p)}>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 sm:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <nav className="grid gap-6">
+                {NavLinkItems.map((item) => (
+                  <NavItem
+                    key={item.content}
+                    selected={pathname.includes(item.to)}
+                    content={item.content}
+                    disabled={item.disabled}
+                    {...item}
+                  />
+                ))}
+              </nav>
+            </SheetContent>
+          </Sheet>
+          <Tooltip>
+            <TooltipTrigger>
+              <div
+                className="hover:cursor-pointer hover:text-slate-300"
+                onClick={() => {
+                  tour.setTourActive(!tour.tourActive)
+                  setOpen(false)
+                }}
+              >
+                take a tour
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="animate-pulse">Tour throw app</p>
+            </TooltipContent>
+          </Tooltip>
+        </div>
       </TooltipProvider>
     </header>
   )
