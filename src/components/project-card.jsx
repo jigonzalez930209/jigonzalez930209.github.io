d9bb1a35a3a5dd545997170d82d5e23ed4df4815
Jose Ivan <jigonzalez930209@gmail.com>
2024-09-10 16:41:21 -0300

    feat: Change project card

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,14 +1,41 @@
 /* eslint-disable react/prop-types */
+import React from 'react'
+
 import { Card } from './ui/card'
 
-const ProjectCard = ({ title, description, link, image, video }) => {
+// Change this component to add tabs, aspect ratio, and resizable panels
+// see how can wrap the content of wikipedia only text to show the content
+// the images from the wikipedia can be shown in the aspect ratio component and a carousel to add more interactivity
+const ProjectCard = ({
+  title,
+  description,
+  link,
+  image: imageProject,
+  video,
+}) => {
+  const [dataWikipedia, setDataWikipedia] = React.useState(null)
+  React.useEffect(() => {
+    fetch(
+      `https://en.wikipedia.org/api/rest_v1/page/summary/${'Sulphur'.replace(
+        ' ',
+        '_'
+      )}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setDataWikipedia(data)
+      })
+  }, [title])
+
+  console.log({ dataWikipedia, title })
+
   return (
     <a href={link} target="_blank" rel="noreferrer">
-      <Card className="grid grid-cols-1 sm:grid-cols-2 gap-5 p-3">
-        <div className=" sm:max-h-min w-full justify-center flex sm:col-span-1 ">
+      <Card className="gap-5 p-3 mb-3 animate-border-glow ">
+        <div className="sm:max-h-min w-full justify-center flex sm:col-span-1 ">
           {video ? (
             <video
-              className="max-h-80 border rounded-md"
+              className="h-[60vw] border rounded-md"
               src={video}
               autoPlay
               loop
@@ -16,14 +43,13 @@ const ProjectCard = ({ title, description, link, image, video }) => {
             />
           ) : (
             <img
-              className="max-h-80 border rounded-md"
-              src={image}
+              className="h-[60vh] border rounded-md"
+              src={imageProject}
               alt={title}
             />
           )}
-          {/* <img className="max-h-80 border rounded-md" src={image} alt={title} /> */}
         </div>
-        <div className="sm:col-span-1 gap-5">
+        <div className="sm:col-span-1 grid gap-5">
           <div className="">{title}</div>
           <div className="">{description}</div>
         </div>
